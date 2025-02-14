d4d8294285e43615ddfdf822b92419a6b3d1048a
ashleysaintlouis <saintlouisashlhey@gmail.com>
2025-02-14 09:00:04 -0300

    Quinta commit

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -4,7 +4,7 @@ const pokeApi = {}
 
 function convertePokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.order
+    pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
 
     const types =  pokeDetail.types.map((typeSlot) => typeSlot.type.name)
@@ -15,7 +15,6 @@ function convertePokeApiDetailToPokemon(pokeDetail) {
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
     
     return pokemon
- 
 }
 
 pokeApi.getPokemonDetail = (pokemon) => {
@@ -24,7 +23,7 @@ pokeApi.getPokemonDetail = (pokemon) => {
     .then(convertePokeApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
+pokeApi.getPokemons = (offset = 0, limit = 4) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     return fetch(url)
         .then((response) => response.json())
