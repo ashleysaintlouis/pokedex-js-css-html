096d98cedd24bf36129f5630b49fdaad78158bd1
ashleysaintlouis <saintlouisashlhey@gmail.com>
2025-02-14 05:36:32 -0300

    Quarta commit

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -8,16 +8,20 @@ function convertePokeApiDetailToPokemon(pokeDetail) {
     pokemon.name = pokeDetail.name
 
     const types =  pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-/*     pokemon.types = 
- */
+    const [type] = types
+    pokemon.types = types
+    pokemon.type = type 
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+    
+    return pokemon
+ 
 }
 
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
     .then((response) => response.json())
-    .then((pokemon) => {
-
-    })
+    .then(convertePokeApiDetailToPokemon)
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
